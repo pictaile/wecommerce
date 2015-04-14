dfadbd01eb788f7e7e341b118b23be96d4f41bbb
Kostya <Kostya@Kostya-pc>
2015-04-14 21:43:26 +0300

    first commit

--- web/js/wecommerce.js
+++ web/js/wecommerce.js
@@ -4,7 +4,14 @@
  * and open the template in the editor.
  */
 
-undefined = true; // mutable
-(function (window, undefined) {
-  alert(typeof undefined); // "undefined", the local identifier
-})(window); // <-- no value passed, undefined by default
+var wecommerce = (function (window, undefined) {
+
+  return {
+      init: function(data){
+            console.log(1);
+         }
+    };
+
+})(); 
+
+
